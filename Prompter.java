96e7a8298cda44e698df651ce1a35249c28ce2a5
dicius933 <dicius933@gmail.com>
2017-08-15 13:00:51 -0400

    Initial commit

---
+++ Prompter.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Prompter{
+  private Game game;
+  
+  public Prompter(Game game){
+    this.game = game;
+  }
+  
+  public boolean promptForGuess() {
+    Scanner scanner = new Scanner(System.in); 
+    boolean isHit = false;
+    boolean isAcceptable = false;
+    
+    do{
+      System.out.print("Enter a letter: ");
+      String guessInput = scanner.nextLine();
+      try{
+        isHit = game.applyGuess(guessInput);
+        isAcceptable = true;
+      } catch(IllegalArgumentException iae){
+        System.out.printf("%s. Please try again. %n", iae.getMessage());
+      }
+    } while (! isAcceptable);
+  return isHit;
+  }
+  
+  public void displayProgress(){
+    System.out.printf(" You jave %d tries left to solve:  %s %n", game.getRemainingTries(), game.getCurrentProgress() ); 
+  }
+  
+  public void displayOutcome(){
+    if (game.isWon()){
+      System.out.printf("Correct, You won the game with %d tries remaining!%n", game.getRemainingTries());
+    } else {
+      System.out.printf("Sorry, you have lost the game. The answer was %s %n", game.getAnswer() );
+      
+    }
+  }
+}
